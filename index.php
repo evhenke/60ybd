7c17101be9ac33abac964049c8fa9cc200d38310
Ev Henke <evhenke@gmx.de>
2013-01-27 15:12:07 +0100

    fist commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta charset="utf-8">
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300|Sorts+Mill+Goudy:400,400italic' rel='stylesheet' type='text/css'>	
+	<link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8"/>
+	<link rel="stylesheet" href="reset.css" type="text/css" charset="utf-8"/>
+	
+	<link rel="icon" href="favicon.ico" />
+	
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>30 + 30</title>
+
+</head>
+
+<body>
+
+<header>
+	<img src="img/salernes_bw.jpg" alt="60" />
+</header>
+
+<div id="sixty" onclick="this.style.visibility='hidden'">
+	<span>60</span>
+</div>
+
+
+<h1><span class="fr">30</span> <span class="de">+ 30</span> <span class="en"> = 60</span></h1>
+<div id="grid">
+  <div class="col-1 fr">
+  	<h3>Nos 60 ans</h3>
+    <p>Fetez nos 60 ans avec nous en la merveilleuse Provence!</p>
+  </div>
+  <div class="col-2 de">
+     <h3>Wir werden 60</h3>
+     <p>Fetez nos 60 ans avec nous en la merveilleuse Provence!</p>
+  </div>
+  <div class="col-3 en">
+  	 <h3>Nos 60 ans</h3>
+     <p>Join us in Southern France for a decent party.</p>
+  </div>
+</div>
+
+</br>
+<h1><span class="fr">Lieu.</span> <span class="de">Location.</span> <span class="en">Location.</span></h1>
+
+</br>
+<h1><span class="fr">Voyage.</span> <span class="de">Anreise.</span> <span class="en">Travel.</span></h1>
+
+
+<!-- div id="footer">
+Wir freuen uns auf Euch!
+</div-->
+
+
+
+<i class="frame-top"><i></i></i><i class="frame-right"></i><i class="frame-bottom"><i></i></i><i class="frame-left"></i>
+
+</body>
+
+</html>
